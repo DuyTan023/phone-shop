7c97f69b87f53e69b00ce92b48e82f2148b7775e
DuyTan023 <phanduytan314@gmail.com>
2026-07-12 21:54:47 +0700

    Initial commit

---
+++ database-sql/data-db.sql
@@ -0,0 +1,38 @@
+
+INSERT INTO brands (name, slug, logo, description) VALUES
+('Apple', 'apple', 'https://res.cloudinary.com/tpreemmc/image/upload/v1783091306/png-apple-logo-9719_bpdina_eqnxk5.png', 'Thương hiệu công nghệ hàng đầu thế giới với dòng sản phẩm iPhone, iPad, MacBook.'),
+('Samsung', 'samsung', 'https://res.cloudinary.com/tpreemmc/image/upload/v1783088954/samsung_5969254_z85bsa_dq6ld6.png', 'Gã khổng lồ công nghệ Hàn Quốc, nổi tiếng với dòng Galaxy S, Galaxy Z và hệ sinh thái đa dạng.'),
+('Oppo', 'oppo', 'https://res.cloudinary.com/tpreemmc/image/upload/v1783089007/oppo-logo-40749_b8eemv_gsmw3p.png', 'Thương hiệu smartphone tập trung vào camera chụp ảnh, thiết kế thời trang và sạc siêu nhanh SuperVOOC.'),
+('Vivo', 'vivo', 'https://res.cloudinary.com/tpreemmc/image/upload/v1783089075/vivo_882711_flyqis_sfkokz.png', 'Hãng điện thoại nổi bật với công nghệ âm thanh hi-fi, khả năng chụp ảnh chuyên nghiệp và thiết kế mỏng nhẹ.'),
+('Xiaomi', 'xiaomi', 'https://res.cloudinary.com/tpreemmc/image/upload/v1783091215/Xiaomi_logo_PNG13_fxx2a6_ufatig.png', 'Mệnh danh là Apple của Trung Quốc, nổi tiếng với cấu hình cao, giá tốt và hệ sinh thái thông minh Mi Home.');
+
+INSERT INTO categories (name, slug) VALUES
+('Điện thoại Chơi Game (Gaming)', 'gaming-phone'),
+('Điện thoại Chụp ảnh chuyên nghiệp', 'camera-phone'),
+('Điện thoại Pin trâu / Sạc siêu nhanh', 'battery-phone'),
+('Điện thoại Phổ thông (Giá rẻ)', 'budget-phone'),
+('Điện thoại Doanh nhân (Cấu hình cao/Sang trọng)', 'premium-phone'),
+('Điện thoại Màn hình gập (Thời thượng)', 'foldable-phone');
+
+INSERT INTO colors (name, hex_code) VALUES
+('Đen Huyền Bí (Phantom Black)', '#1A1A1A'),
+('Trắng Thạch Anh (Quartz White)', '#F5F5F7'),
+('Xanh Đại Dương (Ocean Blue)', '#2E4A62'),
+('Xám Titan (Titanium Gray)', '#8C8C8C');
+
+INSERT INTO storages (value) VALUES
+('64GB'),
+('128GB'),
+('256GB'),
+('512GB'),
+('1TB'),
+('2TB');
+
+INSERT INTO rams (value) VALUES
+('4GB'),
+('6GB'),
+('8GB'),
+('12GB'),
+('16TB');
+
+
